55d207d55020af474af0e0d5ccc0814c6fb76aa0
waltujo <henrialqui@gmail.com>
2024-11-28 12:30:36 -0300

    Live Markdown Preview and Image Preview

---
+++ API/CodePulse/CodePulse.API/Migrations/20241128120643_RenamePublishedDate.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodePulse.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenamePublishedDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PublisehdDate",
+                table: "BlogPosts",
+                newName: "PublishedDate");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PublishedDate",
+                table: "BlogPosts",
+                newName: "PublisehdDate");
+        }
+    }
+}
--- UI/codepulse/src/app/features/blog-post/blogpost-list/blogpost-list.component.ts
+++ UI/codepulse/src/app/features/blog-post/blogpost-list/blogpost-list.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BlogPost } from './../model/blog-post.model';
+import { BlogPostService } from './../services/blog-post.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-blogpost-list',
   templateUrl: './blogpost-list.component.html',
   styleUrls: ['./blogpost-list.component.css']
 })
-export class BlogpostListComponent {
+export class BlogpostListComponent implements OnInit {
+
+  blogPost$?: Observable<BlogPost[]>
+
+  constructor(private blogPostService: BlogPostService){
+
+  }
+
+  ngOnInit(): void {
+    this.blogPost$ = this.blogPostService.getAllBlogPost();
+  }
 
 }
